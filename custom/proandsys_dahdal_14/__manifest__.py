5dee4311c5ee2b69c9c4c3cf532c52b73920d0d1
joseg <jose.figueroa@proandsys.net>
2022-01-25 15:57:08 -0400

    cambios formato dahdal

---
+++ custom/proandsys_dahdal_14/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Proandsys - Dahdal',
+	'version': '1',
+	'author': '[Proandsys]',
+	'website': 'http://www.odooconsultores.cl',
+	'license': 'AGPL-3',
+	'category': 'Localization',
+	'summary': 'Ajustes Dahdal',
+	'description': 
+"""
+
+""",
+	'depends': ['proandsys_report_14'],
+	'data': [
+		'report/account_move.xml',
+	],
+	'installable': True,	
+}
