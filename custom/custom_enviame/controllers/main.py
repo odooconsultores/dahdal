270957555c3d54785aea9d88921371a137a11ad3
ClaraSav <clara.15is.cs@gmail.com>
2023-03-09 12:40:16 -0400

    [FIX] corrigiendo los montos de los calculos subtotales y totales de la orden de venta

--- custom/custom_enviame/controllers/main.py
+++ custom/custom_enviame/controllers/main.py
@@ -16,7 +16,20 @@ class WebsiteDeliverySend(WebsiteSaleDeliverySend):
         carrier = request.env['delivery.carrier'].sudo().browse(int(post['carrier_id']))
         if order and carrier.is_send and config.get_param('is_env'):
             amount_delivery = order.amount_delivery + 1  # return the changes on l10n_cl_enviame
+            amount_delivery_untax = amount_delivery * 1.01
             order.amount_delivery = amount_delivery * 1.01 * 1.19
-            order.amount_total += order.amount_delivery
-            res['new_amount_delivery'] = Monetary.value_to_html(order.amount_delivery, {'display_currency': order.currency_id})
+            amount_untaxed = amount_tax = 0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal if not line.is_delivery else 0
+                amount_tax += line.price_tax if not line.is_delivery else 0
+            order.amount_untaxed = amount_untaxed + amount_delivery_untax
+            order.amount_tax = amount_tax + (amount_delivery_untax * 0.19)
+            order.amount_total = amount_untaxed + amount_delivery_untax + order.amount_tax
+            res.update({
+                'new_amount_delivery': Monetary.value_to_html(order.amount_delivery, {'display_currency': order.currency_id}),
+                'new_amount_untaxed': Monetary.value_to_html(order.amount_untaxed, {'display_currency': order.currency_id}),
+                'new_amount_tax': Monetary.value_to_html(order.amount_tax, {'display_currency': order.currency_id}),
+                'new_amount_total': Monetary.value_to_html(order.amount_total, {'display_currency': order.currency_id}),
+            })
+
         return res
