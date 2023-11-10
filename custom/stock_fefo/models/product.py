ac871c209c0937f36f2db7c7f6723a503120873e
ClaraSav <clara.15is.cs@gmail.com>
2023-11-10 06:44:13 -0400

    [IMP] que ignore los lotes sin cantidades

--- custom/stock_fefo/models/product.py
+++ custom/stock_fefo/models/product.py
@@ -17,7 +17,8 @@ class ProductProduct(models.Model):
         config_id = self.env['pos.config'].browse(config)
         location = config_id.picking_type_id.default_location_src_id
         quant = self.env['stock.quant']._gather(self, location, strict=False)
-        if quant:
+        if quant and quant.filtered(lambda x: x.quantity > 0):
+            quant = quant.filtered(lambda x: x.quantity > 0)
             lot_name = quant[0].lot_id.name
             return lot_name
         return False
