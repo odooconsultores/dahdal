e67dd92558473de255841658b1dd94a9045db154
ClaraSav <clara.15is.cs@gmail.com>
2023-11-28 15:31:11 -0400

    [IMP]

--- custom/proandsys_dahdal_14/__manifest__.py
+++ custom/proandsys_dahdal_14/__manifest__.py
@@ -12,12 +12,13 @@
 """
 
 """,
-	'depends': ['base', 'proandsys_report_14', 'product', 'website_sale', 'web'],
+	'depends': ['base', 'proandsys_report_14', 'product', 'website_sale', 'web', 'stock_fefo'],
 	'data': [
 		'views/product_views.xml',
 		'views/website_templates.xml',
 		'views/res_country_views.xml',
 		'views/webclient_templates.xml',
+		'views/pos_invoice_report.xml',
 	],
 	'installable': True,	
 }
