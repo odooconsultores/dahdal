c5662b7887086c8a4ccf52f7d66b594c07d8f218
ClaraSav <clara.15is.cs@gmail.com>
2023-12-14 07:47:15 -0400

    [FIX] dahdal

--- custom/stock_fefo/models/account_move.py
+++ custom/stock_fefo/models/account_move.py
@@ -8,7 +8,7 @@ class AccountMoveLine(models.Model):
         'stock.production.lot', string='Lot/Serial Number', index=True, store=True,
         ondelete='restrict', check_company=True, compute="_compute_lot_id")
 
-    @api.depends('product_id', 'move_id', 'move_id.pos_order_ids')
+    @api.depends('product_id', 'move_id', 'move_id.pos_order_ids', 'move_id.state')
     def _compute_lot_id(self):
         for line in self:
             lot_id = False
@@ -16,7 +16,7 @@ class AccountMoveLine(models.Model):
             if inv.move_type in ['out_invoice', 'out_refund']:
                 lines = inv.mapped('pos_order_ids.lines')
                 if lines:
-                    pack_lots = lines.filtered(lambda l: l.product_id == line.product_id).mapped('pack_lot_ids.lot_name')
+                    pack_lots = lines.filtered(lambda l: l.product_id == line.product_id and l.qty == line.quantity).mapped('pack_lot_ids.lot_name')
                     lot_id = self.env['stock.production.lot'].search([
                         ('product_id', '=', line.product_id.id),
                         ('name', 'in', pack_lots),
